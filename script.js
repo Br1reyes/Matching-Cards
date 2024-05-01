60401694ccb6642fbfed73b432b3299e1858c221
br1 <145373381+Br1reyes@users.noreply.github.com>
2024-05-01 08:51:32 -0700

    Created using Popcode: https://popcode.org

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 // Game container
 let game = document.querySelector(".game");
+let counter = 0;
+let counterdiv = document.querySelector(".counter");
 // 4 buttons
 let buttonShow = document.querySelector(".show");
 let buttonDouble = document.querySelector(".double");
 let buttonShuffle = document.querySelector(".shuffle");
 let buttonFlip = document.querySelector(".flip");
+let clickedIds = [];
+
 // Array containing image URLs
 
 let url = "https://cdn.glitch.global/305f916c-9128-48d1-be99-03be18f904f8/";
@@ -31,6 +35,8 @@ let cards = [
 buttonShow.onclick = function() {
     // Log message
     console.log("Showing the deck...");
+    counter = counter + 1;
+    counterdiv.innerHTML = ("This game has been played " + counter + " times ");
     // For of loop
     for (let card of cards) {
         game.insertAdjacentHTML("beforeend",
@@ -73,13 +79,13 @@ function shuffle(array) {
 buttonShuffle.onclick = function() {
     shuffle(cards);
     game.innerHTML = "";
-   let i= 0;
+    let i = 0;
     for (let card of cards) {
-        game.insertAdjacentHTML("beforeend", "<div style='background-image:url(" + url + card + ")' id="+i+ "class='card'>");
- i= i + 1;
-}
- console.log("i'm shuffling the cards!");
-buttonShuffle.style.color ="silver";
+        game.insertAdjacentHTML("beforeend", "<div style='background-image:url(" + url + card + ")' class='card' id='" + i + "'>");
+        i = i + 1;
+    }
+    console.log("i'm shuffling the cards!");
+    buttonShuffle.style.color = "silver";
 };
 /* ---------------------------------------------------
 DON'T CHANGE THE Fisher-Yates SHUFFLE FUNCTION BELOW!
@@ -102,11 +108,43 @@ function shuffle(array) {
 
 
 // Button to Flip All Cards
-
-
+buttonFlip.onclick = function() {
+    let i = 0;
+    for (card of cards) {
+        document.getElementById(i).style.backgroundImage = "";
+        i = i + 1;
+    }
+};
 // Here we need a function for clicking on individual cards.
 // (It won't work until we finish writing it.)
 $(document).click(function(event) {
-    // Get the id property of the clicked thing.
-    let clickedId = event.target.id;
-});
+            // Get the id property of the clicked thing.
+            let clickedId = event.target.id;
+            console.log(clickedId);
+            if (clickedId !== "") {
+                document.getElementById(clickedId).style.backgroundImagine = "url('" + url +  cards[clickedId] + "')";
+                clickedIds.push(clickedId);
+                console.log(clickedIds);
+            }
+            if (clickedIds.lengh === 2) {
+                let card1picture = document.getElementById(clickedIds[0]).style.backgroundImage;
+                let card2picture = document.getElementById(clickedIds[1].style.backgroundimage);
+                console.log(card1picture);
+                console.log(card2picture);
+                if (card1picture === card2picture) {
+                    console.log("match");
+                    document.getElementById(clickedIds[0]).id = "";
+                    document.getElementById(clickedIds[1]).id = ""; 
+                                            clickedIds = []; console.log(clickedIds);
+                    }
+
+
+                    else if (clickedIds.lengh > 2) {
+                        document.getElementById(clickedIds[0]).style.backgroundimage = "";
+                        document.getElementById(clickedIds[1]).style.backgroundimage = "";
+                        clickedIds = [];
+                        console.log(clickedIds);
+                    }
+
+            }
+                });
